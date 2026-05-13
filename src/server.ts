afe601cb38c99822afe6f9defc4aa13ebaf223d3
shaqibul-neil <shaqibul.islam.neil@gmail.com>
2026-05-14 00:16:13 +0600

    Setting Up Postgres with Neon Serverless Cloud

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,36 @@
+import dotenv from "dotenv";
 import express from "express";
 import type { TApplication, TRequest, TResponse } from "./types/types";
 
+import { Pool } from "pg";
+
 const app: TApplication = express();
-const port = 5000;
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.text());
+app.use(express.urlencoded({ extended: true }));
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
 
 app.get("/", (req: TRequest, res: TResponse) => {
-  res.send("Express Server with postgres");
+  res.status(200).json({
+    message: "Express Server with postgres",
+    author: "Shaqibul Islam",
+  });
+});
+
+app.post("/", async (req: TRequest, res: TResponse) => {
+  const { name, email, password } = req.body;
+  res.status(201).json({
+    message: "Created",
+    data: {
+      name,
+      email,
+    },
+  });
 });
 
 app.listen(port, () => {
